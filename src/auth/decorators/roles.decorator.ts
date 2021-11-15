e1c05886734d9fc7c5c92dc68110c3d52d5ef580
julio <juliomenaya@gmail.com>
2021-11-14 23:16:33 -0600

    get orders by client and tweaks

--- src/auth/decorators/roles.decorator.ts
+++ src/auth/decorators/roles.decorator.ts
@@ -4,4 +4,4 @@ import { Role } from "../models/roles.models";
 
 export const ROLES_KEY = 'roles';
 
-export const Roles = (...roles: Role[]) => SetMetadata(ROLES_KEY, true);
+export const Roles = (...roles: Role[]) => SetMetadata(ROLES_KEY, [...roles]);
--- src/users/services/orders.service.ts
+++ src/users/services/orders.service.ts
@@ -21,6 +21,10 @@ export class OrdersService {
     return this.orderModel.findById(id);
   }
 
+  async ordersByCustomer(customer: string) {
+    return await this.orderModel.find({ customer }).exec();
+  }
+
   create(data: CreateOrderDto) {
     const newModel = new this.orderModel(data);
     return newModel.save();
